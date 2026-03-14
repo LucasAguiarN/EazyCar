1e3d4c8ebdbb039b2cda21a98023407ee2c4eabf
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2026-03-14 18:35:23 -0300

    add deletar, rascunho de login e select cliente

--- Controllers/cliente_controller.py
+++ Controllers/cliente_controller.py
@@ -19,25 +19,49 @@ class ClienteController:
         complemento = dados.get['complemento']
 
         if not nome or not cpf or not email or not senha or not celular or not cep or not endereco or not numero:
-            return jsonify({"error": "Formulário incompleto"}), 400
+            return jsonify({"error": "Formulário incompleto!"}), 400
         
         registro_clientes = Cliente.query.filter_by(cpf=cpf)
         if not registro_clientes:
             return jsonify({"erro": "CPF já Cadastrado!"}), 409
         
-        cliente = Cliente(
-            nome = nome
-            cpf = cpf
-            email = email
-            senha = senha
-            celular = celular
-            cep = cep
-            endereco = endereco
-            numero = numero
+        novo_cliente = Cliente(
+            nome = nome,
+            cpf = cpf,
+            email = email,
+            senha = senha,
+            celular = celular,
+            cep = cep,
+            endereco = endereco,
+            numero = numero,
             complemento = complemento
         )
 
-        db.session.add(cliente)
+        db.session.add(novo_cliente)
         db.session.commit()
 
         return jsonify({"mensagem": "Cadastro realizado com Sucesso!"}), 201
+    
+    def exibir_cliente(email):
+        cliente = Cliente.query.filter_by(cpf=cpf).first()
+        if cliente:
+            return jsonify(cliente.para_dicionario()), 200
+        else:
+            return jsonify({"erro": "CPF não encontrado!"})
+        
+    def login_cliente(email, senha):
+        cliente = Cliente.query.filter_by(email=email).first()
+        if cliente and cliente.senha == senha:
+                return jsonify({"mensagem": "Login realizado com sucesso!"}), 200
+        else:
+            return jsonify({"erro": "Email ou senha inválidos!"}), 401
+        
+    def deletar_cliente(email):
+        cliente = Cliente.query.filter_by(email=email).first()
+        if not cliente:
+            return jsonify({"erro": "Email não cadastrado!"}), 404
+        
+        db.session.delete(cliente)
+        db.session.commit()
+
+        return jsonify({"mensagem": "Conta deletada com sucesso!"}), 200
