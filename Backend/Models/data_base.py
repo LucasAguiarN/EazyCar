9fc0ad6df4925b0c085df96b91d009c0b2137106
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2026-03-17 23:35:29 -0300

    banco de dados com senhas testes para rodar em debug

--- Backend/Models/data_base.py
+++ Backend/Models/data_base.py
@@ -5,14 +5,7 @@ db = SQLAlchemy()
 
 def iniciar_db(app):
 
-    user = os.getenv('db_user')
-    password = os.getenv('db_password')
-    host = os.getenv('db_host')
-    port = os.getenv('db_port')
-    database = os.getenv('db_name')
-    uri = f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}'
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = uri
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://mysql+mysqlconnector://root:senha@mysql:3306/aluguel_carros'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
