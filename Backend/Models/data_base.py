3d930faa0a60a0fb56a65e4e9ddf5684d6299abe
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2026-03-18 00:21:07 -0300

    fix banco de dados rodando com senhas testes

--- Backend/Models/data_base.py
+++ Backend/Models/data_base.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def iniciar_db(app):
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://mysql+mysqlconnector://root:senha@mysql:3306/aluguel_carros'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:123@mysql:3306/aluguel_carros'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
