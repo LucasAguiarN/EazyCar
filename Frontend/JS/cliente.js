5c96a9bbe2f31e864437d133610849e4a7e147c4
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2026-03-17 23:31:16 -0300

    add requisição para cadastro no script e html

---
+++ Frontend/JS/cliente.js
@@ -0,0 +1,56 @@
+function validar_dados(){
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let cpf = document.getElementById("cpf").value;
+    let senha = document.getElementById("senha").value;
+    let celular = document.getElementById("celular").value;
+    let cep = document.getElementById("cep").value;
+    let endereco = document.getElementById("endereco").value;
+    let numero = document.getElementById("numero").value;
+
+    if (!nome || !email || !cpf || !senha || !celular || !cep || !endereco || !numero){
+        alert("Preencha Todos os Campos");
+    }
+    else{
+        enviar_cadastro(nome, email, cpf, senha, celular, cep, endereco, numero);
+    }
+}
+
+async function enviar_cadastro(nome, email, cpf, senha, celular, cep, endereco, numero) {
+    let complemento = document.getElementById("complemento").value;
+
+    let dados = {
+        "nome": nome,
+        "cpf": cpf,
+        "email": email,
+        "senha": senha,
+        "cpf": cpf,
+        "celular": celular,
+        "cep": cep,
+        "endereco": endereco,
+        "numero": numero,
+        "complemento": complemento
+    }
+    
+    try{
+        let response = await fetch(
+            "http://localhost:5000/clientes", {
+                method: "POST",
+                body: JSON.stringify(dados),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+    
+        if (!response.ok) {
+            let resposta = await response.json();
+            throw new Error(resposta);
+        }
+        alert("Cadastro Realizado com Sucesso!");
+        window.location.href = "../pages/login.html";
+    }
+    catch (error) {
+        console.error("Erro:", error);
+    }
+}
