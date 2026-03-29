ea042053f3c8d2e8f2f878150d0398d52e4ff981
Lucas-Aguiar-Nunes <lucas_aguiar_nunes@outlook.com>
2026-03-29 17:41:36 -0300

    front inicial de funcionario

---
+++ Frontend/JS/cadastro_funcionario.js
@@ -0,0 +1,40 @@
+async function enviar_cadastro() {
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let cpf = document.getElementById("cpf").value;
+    let senha = document.getElementById("senha").value;
+    let celular = document.getElementById("celular").value;
+    let cargo = document.getElementById("cargo").value;
+
+    let dados = {
+        nome: nome,
+        cpf: cpf,
+        email: email,
+        senha: senha,
+        celular: celular,
+        cargo: cargo
+    };
+
+    try {
+        let response = await fetch("http://localhost:5000/funcionarios", {
+            method: "POST",
+            body: JSON.stringify(dados),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        let resposta = await response.json();
+
+        if (!response.ok) {
+            throw new Error(resposta.mensagem || "Erro ao cadastrar funcionário");
+        }
+
+        alert("Cadastro realizado com sucesso!");
+        window.location.href = "../pages/login_sistema.html";
+
+    } catch (error) {
+        console.error("Erro:", error);
+        alert(error.message);
+    }
+}
